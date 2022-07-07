516ee8967c7f065af34954ff7cfb93e0dba4ab42
Nico Neiman <nico.neiman@neofinancial.com>
2022-07-07 10:46:24 -0500

    mocks

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,23 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 
-const server = new ApolloServer({ typeDefs });
+const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(6)],
+  }),
+  Track: () => ({
+    id: () => "track 01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => ({
+      name: "Grumpy Cat",
+      photo: "https://unsplash.com/photos/75715CVEJhI",
+    }),
+    thumbnail: () => "https://unsplash.com/photos/75715CVEJhI",
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
+const server = new ApolloServer({ typeDefs, mocks: mocks });
 
 server
   .listen()
