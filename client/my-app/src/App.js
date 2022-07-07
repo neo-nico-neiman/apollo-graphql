3cfe95c59ebffbc1fa049a68bc941ec1f52b2f8d
Nico Neiman <nico.neiman@neofinancial.com>
2022-07-07 14:08:48 -0500

    useQuery

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,7 +1,48 @@
 import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 function App() {
-  return <div className="App">Hey</div>;
+  const { data, loading, error } = useQuery(TRACKS);
+
+  return loading ? (
+    <span>Loading...</span>
+  ) : (
+    <div className="App">
+      {data?.tracksForHome?.map((track) => (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <img
+            src={track.thumbnail}
+            alt={track.author.name}
+            width={200}
+            height={20}
+          />
+          <span>{track.id}</span>
+          <span>{track.title}</span>
+          <span>
+            {`length: ${track.length} - module count: ${track.modulesCount}`}
+          </span>
+          <span>{track.author.name}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
